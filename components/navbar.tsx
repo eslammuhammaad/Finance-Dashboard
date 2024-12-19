7daabf112a4f7586f6d64a688428ee5ef208797f
eslammuhammaad <eslammuhammaadd.67@gmail.com>
2024-12-19 02:32:17 +0200

    remove unused component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,25 +1,17 @@
 import React from "react";
-import { UserNav } from "./user-nav";
 import { LanguageToggle } from "./language-toggle";
 
 export default function Navbar() {
   return (
     <header className="border-b border-zinc-800 bg-zinc-950">
       <div className="flex h-16 items-center px-6 justify-between">
-        {/* Left Side: Logo or Title */}
         <div className="flex items-center space-x-2">
           <span className="text-lg font-semibold text-zinc-50">
             First Finance
           </span>
-          {/* Dropdown Icon (Optional) */}
           <span className="text-zinc-400 text-sm">▼</span>
         </div>
-
-        {/* Center: Language Selector */}
-
-        {/* Right Side: User Navigation */}
         <div className="flex items-center space-x-4">
-          {/* Profile Icon */}
           <div className="text-zinc-200">
             <LanguageToggle />
           </div>
