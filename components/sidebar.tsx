846541b8d2e778366bad638f8b1b7332d786f434
eslammuhammaad <eslammuhammaadd.67@gmail.com>
2024-12-20 14:03:01 +0200

    finalize finance dashboard

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -15,7 +15,6 @@ import {
 export default function Sidebar() {
   return (
     <aside className="fixed top-0 left-0 flex flex-col items-center bg-black text-white h-screen w-16 py-4 z-50">
-      {/* Logo */}
       <div className="mb-8">
         <img
           src="https://s3-alpha-sig.figma.com/img/f50a/71c6/a2f6d1a94ff7c3e42c10148d147be41c?Expires=1735516800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=ZJjYP8d0VPHnbtmixwqdr4Pzwo95qNtl-RZml081M5QBRwJk2PZ8GY1Gfr-5rvYrvWnAGciJKUrUs5qOgwMoi-vOfnDE7dydCtZ3MUdWPgCGX-SY4u3nC69NyEGTO-t6f6vP2WrGRC7v-XN~h3W1Mb2~QEQ~YrQS-5tdF0lvCV5XfCO-rr93X5zBFKbSb45ygnergHWRf4Vhcy2h5OROSzM6SIWdwqDmN03nVmRb~tP5~Vy4ZSJuIJbc5ykGJK3QZeLizZ8M3AQnXJD4CPKC0wXzf~bp6EMtxxDC01Pwl6V4mHv4Kv41gaQMkHq84n0wZeZHDrhJmF~hBgbU8xrijQ__"
@@ -23,7 +22,6 @@ export default function Sidebar() {
         />
       </div>
 
-      {/* Icons */}
       <nav className="flex-1 space-y-6">
         <NavItem icon={LayoutDashboard} />
         <NavItem icon={BarChart} />
@@ -35,18 +33,22 @@ export default function Sidebar() {
         <NavItem icon={AlertCircle} />
       </nav>
 
-      {/* Footer */}
       <div className="mt-auto">
-      <div className="p-2 hover:bg-gray-700 rounded-md cursor-pointer text-red-600">
-        <NavItem icon={DoorOpen}  />
+        <div className="p-2 hover:bg-gray-700 rounded-md cursor-pointer text-red-600">
+          <NavItem icon={DoorOpen} />
         </div>
-
       </div>
     </aside>
   );
 }
 
-function NavItem({ icon: Icon, className }: { icon: React.ComponentType<{ size: number }>; className?: string }) {
+function NavItem({
+  icon: Icon,
+  className,
+}: {
+  icon: React.ComponentType<{ size: number }>;
+  className?: string;
+}) {
   return (
     <div
       className={`p-2 hover:bg-gray-700 rounded-md cursor-pointer ${className}`}
